d45f68d6ee8f1b20735acc852a19bd8b9534af29
itomi <196012@student.pwr.wroc.pl>
2014-04-06 01:47:34 +0200

    Added communication parts

--- src/pl/pwr/sztuczneoko/communication/BluetoothCommunication.java
+++ src/pl/pwr/sztuczneoko/communication/BluetoothCommunication.java
@@ -1,26 +1,36 @@
 package pl.pwr.sztuczneoko.communication;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 
 import android.bluetooth.BluetoothAdapter;
 
 public class BluetoothCommunication implements Communication{
 
+	private static final String PRECONDITION = "Bluetooth adapted preconditions are not checked.";
+
 	private static BluetoothAdapter ADAPTER = BluetoothAdapter.getDefaultAdapter();
 	
 	private Set<Device> cachedDevices = new ConcurrentSkipListSet<Device>();
 	
+	private BluetoothCommunication() {}
+	
 	@Override
 	public Set<Device> getCachedDevices() {
-		// TODO Auto-generated method stub
-		return null;
+		return ImmutableSet.copyOf(cachedDevices);
 	}
 
 	@Override
 	public Set<Device> getDevicesByInquiry() {
-		// TODO Auto-generated method stub
-		return null;
+		Set<Device> discoveredByInquiry = new HashSet<Device>();
+		
+		
+		return ImmutableSet.copyOf(cachedDevices);
 	}
 
 	@Override
@@ -33,6 +43,26 @@ public class BluetoothCommunication implements Communication{
 	@Override
 	public void prepareCommunicationBundle() {
 		
+	}
+
+	private void checkPreconditions() throws Exception {
+		Set<String> preconditions = gatherPreconditions();
+		if(!preconditions.isEmpty()) {
+			throw new Exception(PRECONDITION);
+		}
+	}
+
+	private Set<String> gatherPreconditions() {
+		Set<String> preconditions = new HashSet<String>();
+		if(ADAPTER == null) {
+			preconditions.add("Adapter is not present.");
+		} else {
+			if( !ADAPTER.isEnabled()) {
+				preconditions.add("Adapter is not enabled.");
+			}
+			//more options here in the future
+		}
+		return preconditions;
 	}
 	
 }
